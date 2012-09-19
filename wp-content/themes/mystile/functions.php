30696ce98cc79492301b7536306bf21d21f52537
Rhev <rev_gutierrez@yahoo.com>
2012-09-19 16:35:56 +0800

    add user restrictions

--- wp-content/themes/mystile/functions.php
+++ wp-content/themes/mystile/functions.php
@@ -40,38 +40,45 @@ foreach ( $includes as $i ) {
 /* You can add custom functions below */
 /*-----------------------------------------------------------------------------------*/
 
+/*add_role('shop_owner', 'Shop Owner', array( 'manage_shop' ));
+$role = get_role('shop_owner');
+$role->remove_cap('edit_others_pages');
+$role->remove_cap('edit_others_posts');
+$role->remove_cap('delete_others_pages');
+$role->remove_cap('delete_others_posts');
+
 function testing() {
   echo 'Hello World!';
 }
 
-add_action( 'admin_head', 'testing' ); 
+add_action( 'admin_head', 'testing' ); */
 
 
-
-function remove_menu_items() {
-  global $menu;
-  $restricted = array(__('Links'), __('Comments'), __('Media'),
-  __('Plugins'), __('Tools'), __('Settings'), __('Pages'), __('Appearance'), __('Posts'));
-  end ($menu);
-  while (prev($menu)){
-    $value = explode(' ',$menu[key($menu)][0]);
-    if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){
-      unset($menu[key($menu)]);}
-    }
-  }
-
-add_action('admin_menu', 'remove_menu_items');
-
-//Remove Menus
-function remove_submenus() {
-  global $submenu;
-  unset($submenu['users.php'][5]); //All Users
-  unset($submenu['users.php'][10]); //Add Users
-  unset($submenu['index.php'][10]); // Updates'.
+if (!current_user_can('update_core')) {
+	function remove_menu_items() {
+	  global $menu;
+	  $restricted = array(__('Links'), __('Comments'), __('Media'),
+	  __('Plugins'), __('Tools'), __('Settings'), __('Pages'), __('Appearance'), __('Posts'));
+	  end ($menu);
+	  while (prev($menu)){
+		$value = explode(' ',$menu[key($menu)][0]);
+		if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){
+		  unset($menu[key($menu)]);}
+		}
+	  }
+	
+	add_action('admin_menu', 'remove_menu_items');
+	
+	//Remove Menus
+	function remove_submenus() {
+	  global $submenu;
+	  unset($submenu['users.php'][5]); //All Users
+	  unset($submenu['users.php'][10]); //Add Users
+	  unset($submenu['index.php'][10]); // Updates'.
+	}
+	
+	add_action('admin_menu', 'remove_submenus');
 }
-
-add_action('admin_menu', 'remove_submenus');
-
 /*-----------------------------------------------------------------------------------*/
 /* Don't add any code below here or the sky will fall down */
 /*-----------------------------------------------------------------------------------*/
